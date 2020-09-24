16ad20fc65e5054e530e6219a11de876258bdab6
Daniel Bullock <dnbulloc@iu.edu>
2020-09-24 12:01:42 -0400

    created OCT2NIFTI function affine creation/translation appears not to function, debug this

---
+++ OCTcode/OCT2NIFTIwrapper.m
@@ -0,0 +1,63 @@
+function OCTNIFTIout=OCT2NIFTIwrapper(path2RawSubjectCSV,path2CentroidFile)
+%OCTNIFTIout=OCT2NIFTIwrapper(path2RawSubjectCSV,path2CentroidFile)
+%
+%  This function converts a ******* [company name]  ***** [format] raw data
+%  output to an ad hoc nifti file.  Includes an affine transform for
+%  alignment purposes.  Uses standard nifti_tools resources.  In essence,
+%  this is the equivalent of an ACPC alignment for ******* [company name]
+%  ***** [format] raw data using the centroid data in [path2CentroidFile]
+%  as the specification of the ACPC origin.
+%
+%  INPUTS
+%
+%  - path2RawSubjectCSV:  Path to this subject's raw csv output from
+%     ******* [company name]  ***** [format], will be parsed by parseCsvExport
+%
+%  - path2CentroidFile= path to the CSV file containing the data indicating
+%     the centroid? of the eye
+%
+%  OUTPUTS
+%
+%  OCTNIFTIout:  an ad hoc nifti with the X dimension corresponding to
+%  ******* [company name]  ***** [format] position data, Y to slice data,
+%  and Z to layer data.  The equivalent of the ACPC origin is the centroid
+%  in path2CentroidFile for the specified subject
+
+%EXAMPLE DATA PATHS
+%path2RawSubjectCSV='/N/u/dnbulloc/Carbonate/OCT_Data/Data/001_OD_T.csv';
+%path2CentroidFile='/N/u/dnbulloc/Carbonate/OCT_Data/Location/Location.csv';
+%% Begin Code
+
+%get current subject/eye ID
+[~,currSubjFile,~]=fileparts(path2RawSubjectCSV);
+
+%load centroidCsv
+centroidTable=readtable(path2CentroidFile);
+
+%get centroid slice from table (Y centroid); it's kind of like pandas
+centroidSlice=centroidTable{find(strcmp(currSubjFile,centroidTable.Filename)),'slice'};
+%same for position (X centroid)
+centroidPosition=centroidTable{find(strcmp(currSubjFile,centroidTable.Filename)),'position'};
+
+%use parseCsvExport to create nifti Data structure
+niftiDataStruc=parseCsvExport(path2RawSubjectCSV);
+
+%make a nifti from this
+%nii = make_nii(img, [voxel_size], [origin], [datatype], [description])
+
+%HUGE ASSUMPTION:  we're going to initialize this as isometric, even though
+%its not actually, we won't be doing rotations, so this will probably be
+%fine
+voxel_size=[1,1,1];
+
+%we're basing the orign off of the x and y centroid, and just arbitrarily
+%setting the Z centroid at 1, as this is the first data entry in that
+%dimension
+origin=[centroidPosition,centroidSlice,1];
+
+%Create a nifit from this information
+%NOTE: this doesn't actually appear to be taking in the origin information
+%appropriately, debug this at a later point.
+OCTNIFTIout = make_nii(niftiDataStruc, voxel_size, origin);
+
+end
