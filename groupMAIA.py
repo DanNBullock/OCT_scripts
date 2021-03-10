57c923ad6b1a9d9bab7a4397f8c847c27d8faaf1
DanNBullock <dnbulloc@iu.edu>
2021-03-10 18:36:07 -0500

    created multiple functions radar plot visualization scatter plot visualization ring mean /stdev table output

--- groupMAIA.py
+++ groupMAIA.py
@@ -13,8 +13,9 @@ import os
 from os import listdir
 from os.path import isfile, join
 import scipy
+import matplotlib.pyplot as plt
 
-currentCSVPath='/Users/plab/Downloads/exampleMAIA/combined_data.txt'
+currentCSVPath='/Users/plab/Downloads/exampleMAIA/004.txt'
 inputDir='/Users/plab/Downloads/exampleMAIA/'
 
 #load a csv and convert the data to radius & value
@@ -42,6 +43,8 @@ def loadAndConvertMAIA_polar(currentCSVPath):
 def multiSubjectDataStruc(inputDir):
     #get the relevant file names
     onlyfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
+    #make robust against the damnable .DS_Store
+    onlyfiles.remove('.DS_Store')
     #arbitrarily make a vector for subject names
     subjList= ['subj_'+str(f) for f in range(len(onlyfiles))]
     #create a structure to hold all of the dataframes
@@ -51,8 +54,7 @@ def multiSubjectDataStruc(inputDir):
     mergedFrame=pd.concat(concatInput)
     return mergedFrame
 
-distanceKernel=2
-currentCoord=[0,0]
+
 def coordKernelMeanSD(currentCoord,mergedFrame,distanceKernel):
     compareCoords=(np.asarray([mergedFrame.x_deg,mergedFrame.y_deg]).T)
     distances=scipy.spatial.distance.cdist(np.atleast_2d(currentCoord),compareCoords)
@@ -66,46 +68,103 @@ def coordKernelMeanSD(currentCoord,mergedFrame,distanceKernel):
     weightedVariance=np.average((targetValues[maskCriteriaVec]-weightedAverage)**2, weights=weightVec)
     return weightedAverage, np.sqrt(weightedVariance)
 
+def ringMeanDev(convertedMAIAtable):
+    #
+    floorDists=np.floor(convertedMAIAtable.distance.to_numpy())
+    vals=np.floor(convertedMAIAtable.value.to_numpy())
+    meanVec=np.zeros(10)
+    stdevVec=np.zeros(10)
+    for iDegrees in range(10):
+        meanVec[iDegrees]=np.mean(vals[floorDists==iDegrees])
+        stdevVec[iDegrees]=np.std(vals[floorDists==iDegrees])
+    outframe=pd.DataFrame(np.array([meanVec,stdevVec]).T,columns=['mean','stdev'])
+    return outframe
 
-
-sns.lineplot(data=mergedFrame, x='distance', y='value')
-
-import numpy as np
-import matplotlib.pyplot as plt
+def MAIAradarPlot(convertedMAIAtable):
+    import numpy as np
+    import matplotlib.pyplot as plt
  
-# Using linspace so that the endpoint of 360 is included
-
-# Using linspace so that the endpoint of 360 is included
-vFieldRange = np.radians(np.linspace(0, 360, 40))
-eccentricityRange = np.arange(0, 10, 1)
-
-xCoords=np.outer(np.atleast_2d(eccentricityRange),np.atleast_2d(np.cos(vFieldRange)).T)
-yCoords=np.outer(np.atleast_2d(eccentricityRange),np.atleast_2d(np.sin(vFieldRange)).T)
-sourceShape=xCoords.shape
-
-allMeans=np.zeros(len(np.ravel(xCoords)))
-allSDs=np.zeros(len(np.ravel(xCoords)))
-for iPlotCoords in range(len(np.ravel(xCoords))):
-    currentCoordAverage,currentCoordSD=coordKernelMeanSD([np.ravel(xCoords)[iPlotCoords],np.ravel(yCoords)[iPlotCoords]],mergedFrame,distanceKernel)
-    allMeans[iPlotCoords]=currentCoordAverage
-    allSDs[iPlotCoords]=currentCoordSD
+    # Using linspace so that the endpoint of 360 is included
+    vFieldRange = np.radians(np.linspace(0, 360, 40))
+    eccentricityRange = np.arange(0, 10, 1)
     
-reshapeMeans=np.reshape(allMeans,sourceShape)
-reshapeSDs=np.reshape(allSDs,sourceShape)
+    #get a regular sampling using .outer
+    xCoords=np.outer(np.atleast_2d(eccentricityRange),np.atleast_2d(np.cos(vFieldRange)).T)
+    yCoords=np.outer(np.atleast_2d(eccentricityRange),np.atleast_2d(np.sin(vFieldRange)).T)
+    #figure out how big the dataframe is
+    sourceShape=xCoords.shape
+    
+    #turn the 2d dataframe target into a 1D vector, storage for means and stdev
+    allMeans=np.zeros(len(np.ravel(xCoords)))
+    allSDs=np.zeros(len(np.ravel(xCoords)))
+    
+    #set kernel frame for interpolation.  Values outside of this distance are ignored for interpolation computation
+    distanceKernel=2
+    #compute value for coordinates
+    for iPlotCoords in range(len(np.ravel(xCoords))):
+        currentCoordAverage,currentCoordSD=coordKernelMeanSD([np.ravel(xCoords)[iPlotCoords],np.ravel(yCoords)[iPlotCoords]],convertedMAIAtable,distanceKernel)
+        allMeans[iPlotCoords]=currentCoordAverage
+        allSDs[iPlotCoords]=currentCoordSD
+    
+    reshapeMeans=np.reshape(allMeans,sourceShape)
+    reshapeSDs=np.reshape(allSDs,sourceShape)
 
 
-#r, theta = np.meshgrid(vFieldRange, eccentricityRange)
-#values = np.random.random((actual.size, expected.size))
- 
+    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+    #ax.set_rticks([0.5, 1, 1.5, 2]) 
+    polarFig=ax.contourf(vFieldRange, eccentricityRange, reshapeMeans, levels=np.linspace(np.min(convertedMAIAtable.value.to_numpy()),np.max(convertedMAIAtable.value.to_numpy()),100))
+    plt.colorbar(polarFig)
+    plt.show()
+    return fig
 
- 
-fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
-#ax.set_rticks([0.5, 1, 1.5, 2]) 
-polarFig=ax.contourf(vFieldRange, eccentricityRange, reshapeMeans, levels=np.linspace(np.min(mergedFrame.value.to_numpy()),np.max(mergedFrame.value.to_numpy()),100))
-plt.colorbar(polarFig)
-plt.show()
+def MAIAscatterPlot(convertedMAIAtable):
+    import seaborn as sns
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    fig, axes = plt.subplots(1, 1)
 
+    sns.scatterplot(x="x_deg", y="y_deg", data=convertedMAIAtable, hue="value", cmap="viridis",s=300,ax=axes)
+   
 
-whatisthis=np.random.random((40,80))
+    plt.ylabel('Vertical eccentricity')
+    plt.xlabel('Horizontal eccentricity')
+    plt.show()
+    return fig
 
-compte regular meshgrid but then weight value by distance from current location.  threshold probable, and decay function
+def computeMAIAonDir(inputDir):
+    #get the relevant file names
+    os.makedirs(os.path.join(inputDir,'output/'), exist_ok=True)
+    onlyfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
+    #make robust against the damnable .DS_Store
+    onlyfiles.remove('.DS_Store')
+    #iterate across files in directory
+    for iFiles in range(len(onlyfiles)):
+        #generate current file name
+        currentFileName=os.path.join(inputDir,onlyfiles[iFiles])
+        #create stem for all output files
+        outputFileStem=os.path.join(inputDir,'output/',onlyfiles[iFiles].replace('.txt',''))
+        #do the polar coordinate table conversion
+        convertedMAIAtable=loadAndConvertMAIA_polar(currentFileName)
+        #compute the ring means and save to csv
+        ringMeanTable=ringMeanDev(convertedMAIAtable)
+        ringMeanTable.to_csv(outputFileStem+'_ringMeanTable.csv')
+        #do the radar plot and save to file
+        radarOut=MAIAradarPlot(convertedMAIAtable)
+        radarOut.savefig(outputFileStem+'_radarPlot.png')
+        #do the scatter plot and save to file
+        scatterOut=MAIAscatterPlot(convertedMAIAtable)
+        scatterOut.savefig(outputFileStem+'_scatterPlot.png')
+        
+    #now do it at the group level   
+    allSubjTable=multiSubjectDataStruc(inputDir)
+    groupFileStem=os.path.join(inputDir,'output/group')
+    #compute the ring means and save to csv
+    ringMeanTable=ringMeanDev(allSubjTable)
+    ringMeanTable.to_csv(groupFileStem+'_ringMeanTable.csv')
+    #do the radar plot and save to file
+    radarOut=MAIAradarPlot(allSubjTable)
+    radarOut.savefig(groupFileStem+'_radarPlot.png')
+    #do the scatter plot and save to file
+    scatterOut=MAIAscatterPlot(allSubjTable)
+    scatterOut.savefig(groupFileStem+'_scatterPlot.png')
